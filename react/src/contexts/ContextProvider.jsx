6bab990d84f34c1d2855615253cf60a8ace8b26e
= <kigandaivan32@gmail.com>
2024-05-30 01:19:27 +0300

    adding token and user authorization

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -1,10 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const StateContext = createContext({
-    currentUser: null,
+    user: null,
     token: null
 })
 
-export const ContextProvider = ({
-    
-})
+export const ContextProvider = ({children}) =>{
+const [user, setUser] = useState({});
+const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'))
+const setToken = (token)=>{
+    _setToken(token)
+
+    if(token){
+        localStorage.setItem('ACCESS_TOKEN', token);
+    }
+    else{
+        localStorage.removeItem('ACCESS_TOKEN');
+    }
+}
+
+    return (
+        <StateContext.Provider value={{
+            user,
+            token,
+            setUser,
+            setToken
+
+        }}>
+            {children}
+
+        </StateContext.Provider>
+    )
+}
